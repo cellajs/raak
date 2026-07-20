7d7db735a1b391cfea1943a20122753785bf20b1
flipvanhaaren <flip@shareworks.nl>
2026-07-20 23:54:00 +0200

    fix: align config development

--- shared/config/config.development.ts
+++ shared/config/config.development.ts
@@ -14,10 +14,10 @@ export const development = {
 
   domain: '',
   frontendUrl: 'http://localhost:3000',
-  backendUrl: 'http://localhost:4000',
-  backendAuthUrl: 'http://localhost:4000/auth',
-  yjsUrl: 'http://localhost:4002',
-  mcpUrl: 'http://localhost:4003',
+  backendUrl: 'http://localhost:3000/api',
+  backendAuthUrl: 'http://localhost:3000/api/auth',
+  yjsUrl: 'http://localhost:3000/yjs',
+  mcpUrl: 'http://localhost:3000/mcp',
 
   s3: {
     publicBucket: 'cella-shared-public',
